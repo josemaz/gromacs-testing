c3a8354861f53f5db4f21c1d4a3733c5df7f3839
ch3m <snakelabz@outlook.com>
2022-11-03 13:25:21 -0600

    Adding QE and benchmark of Max Plank

---
+++ GROMACS/max-planck/down.sh
@@ -0,0 +1,8 @@
+# https://www.mpinat.mpg.de/grubmueller/bench
+
+wget "https://www.mpinat.mpg.de/benchMEM"
+mv benchMEM benchMEM.tpr
+wget "https://www.mpinat.mpg.de/benchRIB"
+mv benchRIB benchRIB.tpr
+wget "https://www.mpinat.mpg.de/benchPEP-h"
+mv benchPEP-h benchPEP-h.tpr
