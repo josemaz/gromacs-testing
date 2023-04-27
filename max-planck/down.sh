b46742b69200a4a32be065ce368c8360e9be2d6f
josemaz <47672759+josemaz@users.noreply.github.com>
2023-04-27 15:59:29 -0600

    Update down.sh to restart

--- max-planck/down.sh
+++ max-planck/down.sh
@@ -1,8 +1,5 @@
 # https://www.mpinat.mpg.de/grubmueller/bench
 
-wget "https://www.mpinat.mpg.de/benchMEM"
-mv benchMEM benchMEM.tpr
-wget "https://www.mpinat.mpg.de/benchRIB"
-mv benchRIB benchRIB.tpr
-wget "https://www.mpinat.mpg.de/benchPEP-h"
-mv benchPEP-h benchPEP-h.tpr
+wget -c "https://www.mpinat.mpg.de/benchMEM" -O benchMEM.tpr
+wget -c "https://www.mpinat.mpg.de/benchRIB" -O benchRIB.tpr
+#wget -c "https://www.mpinat.mpg.de/benchPEP-h" -O benchPEP-h.tpr
