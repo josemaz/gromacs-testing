858a14eab0e2364d414adffa3f080dd43ccd8033
josemaz <47672759+josemaz@users.noreply.github.com>
2023-05-25 19:28:42 -0600

    Update runme.sh

--- runme.sh
+++ runme.sh
@@ -1,1 +1,6 @@
-timeout 15m gmx mdrun -s villin.tpr -ntmpi 12 -ntomp 8 -pin on -nsteps 10000000
+
+[[ -d scratch ]] && rm -rf scratch
+mkdir scratch; cd scratch
+
+#timeout 15m gmx mdrun -s ../villin.tpr -ntmpi 12 -ntomp 8 -pin on -nsteps 10000000
+timeout 15m gmx mdrun -s ../dppc.tpr -ntmpi 1 -ntomp $(nproc) -pin on -nsteps 10000000
